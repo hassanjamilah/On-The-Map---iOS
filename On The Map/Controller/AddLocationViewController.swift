8750173bc30a91f48d3266499a970583d3cac5ff
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-02-06 20:19:45 +0800

    Added the view controllers.

---
+++ On The Map/Controller/AddLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddLocationViewController.swift
+//  On The Map
+//
+//  Created by Hassan on 06/02/2020.
+//  Copyright © 2020 Andalus. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
