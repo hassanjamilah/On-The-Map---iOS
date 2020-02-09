b230732e702e3b864662ebf4c5ac899fa7f16457
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-02-09 22:20:11 +0800

    Ready to review.

--- On The Map/Controller/MainTabViewController.swift
+++ On The Map/Controller/MainTabViewController.swift
@@ -2,7 +2,7 @@
 //  MainTabViewController.swift
 //  On The Map
 //
-//  Created by user on 09/02/2020.
+//  Created by Hassan on 09/02/2020.
 //  Copyright © 2020 Andalus. All rights reserved.
 //
 
@@ -10,21 +10,6 @@ import UIKit
 
 class MainTabViewController: UITabBarController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
